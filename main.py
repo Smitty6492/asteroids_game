62e3576e8e19cfd12e743bf08ceb37d17e967716
Smitty6492 <physicsfaith@gmail.com>
2025-07-04 17:00:25 -0500

    G: asteroids created and collisions end game

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -23,11 +26,17 @@ def main():
     # Create groups
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    # Attach groups to classes
     Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables,)  # only updatable
     
     
     # ✅ Create the player in the center of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # GAME LOOP
     while True:
@@ -42,6 +51,15 @@ def main():
         
         # Draw the 
         updatables.update(dt)
+        
+        # Check for collisions between player and asteroids
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("Game over!")
+                #pygame.quit()
+                sys.exit()
+                #return
+        
         screen.fill("black")  # Fill the screen with black
         
         for obj in drawables:
